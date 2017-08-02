e49fc5cc8c59aafc6bd447623a119e3a3060a351
peteratskoruz <peterjoeeraj234@gmail.com>
2017-08-02 15:16:45 +0530

    Add files via upload

---
+++ customjs/custom.js
@@ -0,0 +1,32 @@
+/**
+ * Created by peter joseph on 7/18/2017.
+ */
+
+
+//declaration and assignning value
+
+
+var count=0;
+
+//function counter
+
+function counter(){
+
+    //increamenting value
+
+    count ++;
+
+    //displaying the value
+
+    document.getElementById('demo').innerHTML="Count value is" + " " +count;
+
+}
+
+
+//function invoking
+
+counter();
+
+counter();
+
+counter();
